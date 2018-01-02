f09fcbe00580d4c333eb28fab430ac6637a3d53e
tamiresslelis <tamiresslelis@gmail.com>
2018-01-02 16:03:46 -0200

    tela financiamentol layout

--- www/js/class/financiamento.js
+++ www/js/class/financiamento.js
@@ -4,15 +4,11 @@ var Financiamento = (function() {
   function Financiamento(model) {
 
     //taxa mensal quando se tem a anual
-    console.log("Model:"+model.juros);
-    this.juros = (((Math.pow((1+(model.juros)),1/12)-1)*100)/100);
-    console.log("juros mensal --->"+this.juros);
 
-    //this.juros= model.juros;
+    this.juros = (((Math.pow((1+(model.juros)),1/12)-1)*100)/100);
     this.valorFinanciado = model.valorFinanciado;
+    console.log("valor do financiado"+this.valorFinanciado);
     this.periodoTotal = model.periodo;
-    console.log("juros divide por 100" +this.juros);
-    console.log("valorFinanciado"+this.valorFinanciado);
     this.somaJuros = 0;
     this.somaPrestacoes = 0;
 
@@ -24,30 +20,28 @@ var Financiamento = (function() {
     var taxaJuros = [];
     var grid= [];
     var totalParcelas = [];
+      var amortizacao = (this.valorFinanciado / this.periodoTotal);
     if(this.periodoTotal == 0){
-
       saldoDevedor.push(this.valorFinanciado);
-      console.log("saldoDevedor"+this.valorFinanciado);
+
     }
-    var amortizacao = (this.valorFinanciado / this.periodoTotal);
+
     console.log('Amortizacao'+amortizacao);
-    //console.log('Juros ' + this.juros);
+
     for(var i=0; i <= this.periodoTotal; i++){
     saldoDevedor.push(parseFloat(((this.periodoTotal-i) / this.periodoTotal) * this.valorFinanciado).toFixed(4));
-          console.log('Saldo devedor no periodo' +saldoDevedor[i]);
+
           if(i >0){
             taxaJuros.push(this.juros * saldoDevedor[i-1]);
             this.somaJuros = this.somaJuros + taxaJuros[i];
-            console.log("total de juros " + this.somaJuros);
-            console.log('Juros é igual a ' +taxaJuros[i]);
             prestacao.push(taxaJuros[i] + amortizacao);
             this.somaPrestacoes = this.somaPrestacoes +prestacao[i];
-            console.log("total de prestacoes " + this.somaPrestacoes);
+
           }else{
           taxaJuros.push(0);
           prestacao.push(0);
         }
-        console.log('Prestacao no periodo é igual a ' +prestacao[i]);
+
                grid.push({
                periodo:i,
                taxa:parseFloat(taxaJuros[i]).toFixed(2),
@@ -59,7 +53,6 @@ var Financiamento = (function() {
           })
 
     }
-    console.log('Tamanho do vertor grid:'+grid.length);
     return grid;
   };
 
@@ -77,38 +70,23 @@ var Financiamento = (function() {
 
     if(this.periodoTotal == 0){
       saldoDevedor.push(this.valorFinanciado);
-      console.log("saldoDevedor"+this.valorFinanciado);
+
     }
     for(var i=0; i <= this.periodoTotal; i++){
 
         amortizaoNumerador.push((Math.pow((1+this.juros),i-1)));
-        //console.log('numerador'+amortizaoNumerador[i]);
         amortizacaoDenominador.push(((Math.pow(1+this.juros,this.periodoTotal)-1)));
-        //console.log('denomindor'+amortizacaoDenominador[i]);
-        //DivisaoAmortizacao.push((amortizaoNumerador[i]/amortizacaoDenominador[i]));
-        //console.log('DivisaoAmortizacao '+DivisaoAmortizacao[i]);
         amortizacao.push((amortizaoNumerador[i]/amortizacaoDenominador[i]) * (this.valorFinanciado * this.juros));
-        //console.log('Amortizacao Price'+amortizacao[i]);
-
-        //taxaJuros.push((this.juros*))
-
         saldoDevedorNumerador.push(1-(Math.pow((1+this.juros),(this.periodoTotal-i)*(-1))));
-        console.log('Saldo Devedor Numerador :'+saldoDevedorNumerador[i]);
         saldoDevedorDenominador.push(1-(Math.pow((1+this.juros),(this.periodoTotal)*(-1))));
-        console.log('Saldo Devedor denomindor :'+ saldoDevedorDenominador[i]);
         saldoDevedor.push((saldoDevedorNumerador[i]/saldoDevedorDenominador[i]) * this.valorFinanciado);
-        console.log('Saldo Devedor '+saldoDevedor[i]);
-
 
         if(i >0){
         taxaJuros.push(this.juros * saldoDevedor[i-1]);
         this.somaJuros = this.somaJuros + taxaJuros[i];
-        console.log("total de juros " + this.somaJuros);
-        console.log('Juros: if' +taxaJuros[i]);
         prestacao.push(taxaJuros[i]+amortizacao[i]);
         this.somaPrestacoes = this.somaPrestacoes +prestacao[i];
-        console.log("total de prestacoes: " + this.somaPrestacoes);
-        console.log('Valor da prestacao: '+prestacao[i]);
+
 
         }else{
         taxaJuros.push(0);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,68 @@
 angular.module('starter.controllers', [])
 
-.controller('InicioCtrl', function($scope) {})
+.controller('InicioCtrl', function($scope,){
+
+})
+
 
 .controller('FinanciamentoCtrl', function ($scope, $ionicPopup){
+/*  $scope.mascara = function() {
+    $('#currency').maskMoney();
+  };*/
 
-  function mascara() {
-      $('#currency').maskMoney();
-    }
+  $scope.model = {
+    valorFinanciado: '',
+    juros: '',
+    periodo: '',
+    tabela: '',
+    somaJuros: 0,
+    somaPrestacoes: 0
 
+  };
+  $scope.alertCampos = function() {
+    $ionicPopup.alert({
+      title: 'ATENÇÃO',
+      content: 'Preencha todos os campos!'
+    }).then(function (res) {
+      console.log('Test Alert Box');
+    });
+  }
 
+  $scope.alertTabela = function(){
+    $ionicPopup.alert({
+      title: 'Tabela gerada',
+      content: 'Deslize para baixo e veja!'
+    }).then(function (res) {
+      console.log('Test Alert Box');
+    });
+  }
+
+$scope.submit=function(){
+var financiamento = new Financiamento($scope.model);
+
+            if($scope.model.tabela == "none"){
+              $scope.alertCampos();
+            }else{
+                  if($scope.model.tabela == "sac") {
+                              var tabe1 = 1;
+                              $scope.grid = financiamento.sac();
+                  }
+                  if ($scope.model.tabela == "price") {
+                     if($scope.model.juros == 0){
+                       $ionicPopup.alert({
+                         title: 'ATENÇÃO',
+                         content: 'Só é possível efetuar os calculos com taxa de juros diferente de 0 para a Tabela PRICE!'
+                       }).then(function (res) {
+                         console.log('Test Alert Box');
+                       });
+                     }else{
+                       var tabe2 = 1;
+                       $scope.grid = financiamento.price();}
+                  }
+                  if (tabe1 || tabe2 == 1) {
+                    $scope.alertTabela();}
+                  }
+      }
 })
 
 .controller('AccountCtrl', function($scope) {
